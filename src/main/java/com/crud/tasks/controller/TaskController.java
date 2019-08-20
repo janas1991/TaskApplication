ac9f49a2767787d9c65682febef559e4124fd894
janas1991 <michaljanas1991@gmail.com>
2019-08-20 21:43:30 +0200

    Poprawiłem ,
    ponadto dla cwiczen zrobilem implementacje metody getTask, która miała by się zajmować pobieraniem elementu z Listy
    oraz metode createTask,

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,9 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,20 +20,30 @@ public class TaskController {
 
     @GetMapping(value = "{id}")
     public TaskDto getTask(@PathVariable("id") long taskId) {
-        return new TaskDto(taskId, "test_title", "test_content");
+        List<TaskDto> tmpList = getTasks();
+        TaskDto tmpTaskDto = null;
+        for (TaskDto taskDto : tmpList) {
+            if (taskDto.getId() == taskId) {
+                tmpTaskDto = new TaskDto(taskDto.getId(), taskDto.getTitle(), taskDto.getContent());
+                return tmpTaskDto;
+            } else {
+                return null;
+            }
+        }
+        return tmpTaskDto;
     }
 
-    @DeleteMapping(value = "/delateTask/{id}")
-    public void delateTask(@PathVariable("id") long taskId) {
+    @DeleteMapping(value = "task/{id}")
+    public void deleteTask(@PathVariable("id") long taskId) {
     }
 
-    @PutMapping(value = "/updateTask/{id}")
-    public TaskDto updateTask(@PathVariable("id") long taskId ,  TaskDto taskDto) {
-        return new TaskDto(taskId, "test_title", "test_content");
+    @PutMapping(value = "task/{id}")
+    public TaskDto updateTask(@PathVariable("id") long taskId, @Valid TaskDto taskDto) {
+        return new TaskDto(taskId, taskDto.getTitle(), taskDto.getContent());
     }
 
-    @PostMapping(value = "/createTask/{tasksDto}")
-    public void createTask(@PathVariable("tasksDto") TaskDto taskDto) {
-
+    @PostMapping(value = "task/{id},{title},{content}")
+    public Task createTask(@PathVariable("id") long taskId, @PathVariable("title") String title, @PathVariable("content") String content) {
+        return new Task(taskId, title, content);
     }
 }
