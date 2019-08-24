07d2d36d97f9740d1027fb689c3dab91e2073a83
janas1991 <michaljanas1991@gmail.com>
2019-08-24 15:42:49 +0200

    Probując odczytac dane poprzez PostMana albo bezposrednio z storny internetowej wyskakuje mi ze nie mozna nawiazac polaczeni ze stroną

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -2,20 +2,27 @@ package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.services.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/TaskController")
+@RequestMapping("tasks")
 public class TaskController {
 
-    @GetMapping(value = "/getTasks")
+    @Autowired
+    DbService dbService;
+
+    @Autowired
+    TaskMapper taskMapper;
+
+    @GetMapping(value = "")
     public List<TaskDto> getTasks() {
-        return new ArrayList<TaskDto>();
+        return taskMapper.mapToListTaskDto(dbService.getAllTasks());
     }
 
     @GetMapping(value = "{id}")
@@ -33,16 +40,16 @@ public class TaskController {
         return tmpTaskDto;
     }
 
-    @DeleteMapping(value = "task/{id}")
+    @DeleteMapping(value = "/{id}")
     public void deleteTask(@PathVariable("id") long taskId) {
     }
 
-    @PutMapping(value = "task/{id}")
+    @PutMapping(value = "/{id}")
     public TaskDto updateTask(@PathVariable("id") long taskId, @Valid TaskDto taskDto) {
         return new TaskDto(taskId, taskDto.getTitle(), taskDto.getContent());
     }
 
-    @PostMapping(value = "task/{id}")
+    @PostMapping(value = "/{id}")
     public Task createTask(@PathVariable("id") long taskId, String title, String content) {
         return new Task(taskId, title, content);
     }
