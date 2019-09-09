8d26c7068e133204e67586f04d187f1f98e1f5ac
janas1991 <michaljanas1991@gmail.com>
2019-09-09 22:45:30 +0200

    Poprawione

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -4,12 +4,15 @@ import com.crud.tasks.domain.TrelloBoardDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Optional.ofNullable;
 
@@ -31,19 +34,24 @@ public class TrelloClient {
     @Autowired
     RestTemplate restTemplate;
 
-    public List<TrelloBoardDto> getTrelloBoards() {
+    public List<Optional<TrelloBoardDto[]>> getTrelloBoards() {
 
         TrelloBoardDto[] boardsResponse = restTemplate.getForObject(getURIToTrello(trelloAppKey, trelloToken, trelloApiEndPoint, trelloUsername), TrelloBoardDto[].class);
-
-        return Arrays.asList(ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]));
+        try {
+            return Arrays.asList(ofNullable(boardsResponse));
+        } catch (RestClientResponseException e) {
+            throw new RestClientResponseException(e.getMessage(), e.getRawStatusCode(), e.getStatusText(), e.getResponseHeaders(), e.getResponseBodyAsByteArray(), Charset.defaultCharset());
+        }
     }
 
-    private URI getURIToTrello(String trelloAppKey, String trelloToken, String trelloApiEndPoint, String trelloUsername) {
+    private URI getURIToTrello(String trelloAppKey, String trelloToken, String trelloApiEndPoint, String
+            trelloUsername) {
 
         URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndPoint + "/members/" + trelloUsername + "/boards")
                 .queryParam("key", trelloAppKey)
                 .queryParam("token", trelloToken)
-                .queryParam("fields", "name,id").build().encode().toUri();
+                .queryParam("fields", "name,id")
+                .queryParam("lists", "all").build().encode().toUri();
 
         return url;
     }
