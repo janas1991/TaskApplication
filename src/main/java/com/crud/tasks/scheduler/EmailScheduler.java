d66036337b06e8edbf71cc04114197cf2aede607
Janas1991 <michaljanas1991@gmail.com>
2019-09-16 20:05:06 +0200

    Napisany warunek w klasie EmailScheduler - poprawiona wersja , wczesniej sprawdzalem bo mi wyjatek wyskakiwal :)

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -20,13 +20,13 @@ public class EmailScheduler {
 
     @Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
-      //long size = taskRepository.count();
-       // if (size == 1 || size == 0) {
-      //      taskCount = "task";
-       // } else {
-       //     taskCount = "tasks";
-       // }
-        simpleEmailService.send(new Mail("michaljanas1991@gmail.com", SUBJECT, "Currently in database you got: ", "michaljanas1991@gmail.com"));
+      long size = taskRepository.count();
+        if (size == 1 || size == 0) {
+            taskCount = "task";
+        } else {
+            taskCount = "tasks";
+        }
+        simpleEmailService.send(new Mail("michaljanas1991@gmail.com", SUBJECT, "Currently in database you got: " + size + " " + taskCount, "michaljanas1991@gmail.com"));
     }
 
 }
