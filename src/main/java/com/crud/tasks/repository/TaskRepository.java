8baf9760bac68e5d562f55b0ecd6f77e22e485ec
Janas1991 <michaljanas1991@gmail.com>
2019-09-16 20:01:20 +0200

    Napisany warunek w klasie EmailScheduler

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,16 +1,19 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
-
+    @Override
     List<Task> findAll();
-
-    Optional<Task> findById(long id);
-
+    @Override
+    Optional<Task>findById(Long id);
+    @Override
     Task save(Task task);
+    @Override
+    long count();
 }
