e68fbb6a12490e3db1760b16cd08cd8511eea1d5
Janas1991 <michaljanas1991@gmail.com>
2019-12-05 20:49:38 +0100

    created_trello_card-mail

---
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -0,0 +1,20 @@
+
+package com.crud.tasks.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class AdminConfig {
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${admin.mail}")
+    private String adminMail;
+
+    @Value("{admin.company}")
+    private String adminCompany;
+}
