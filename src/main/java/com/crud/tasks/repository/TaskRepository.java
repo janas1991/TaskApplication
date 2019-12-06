ae0276cf1d8940e2f22e4425f887d8a4ea4845de
Janas1991 <michaljanas1991@gmail.com>
2019-12-06 12:48:46 +0100

    W nastepnym zadaniu mam refactoring calej apki, to wtedy przegladne i pozmieniam wszedzie gdzie sie da na wstrzykiwanie na przez konstruktor :)

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Component
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
--- src/main/java/com/crud/tasks/services/TrelloService.java
+++ src/main/java/com/crud/tasks/services/TrelloService.java
@@ -1,11 +1,14 @@
 package com.crud.tasks.services;
 
+import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.mapper.CreatedTrelloCardDto;
+import com.crud.tasks.repository.TaskRepository;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +20,19 @@ public class TrelloService {
     @Autowired
     TrelloClient trelloClient;
 
+    @Autowired
+    SimpleEmailService simpleEmailService;
+
+    @Autowired
+    AdminConfig adminConfig;
+
+    @Autowired
+    TaskRepository taskRepository;
+
+
+
     private static final String SUBJECT = "Tasks: New Trello Card";
+
 
     public List<TrelloBoardDto> fetchTrelloBoards() {
         return trelloClient.getTrelloBoards();
@@ -25,6 +40,12 @@ public class TrelloService {
 
     public CreatedTrelloCardDto createdTrelloCard(final TrelloCardDto trelloCardDto){
         CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
+        ofNullable(newCard).ifPresent(card->simpleEmailService.send(simpleEmailService.createMimeMessage(new Mail(adminConfig.getAdminMail(),SUBJECT,"New card: " + card.getName() + " has been created on your Trello account","michaljanas1991@gmail.com"))));
         return newCard;
+    }
+
+    @Scheduled(fixedDelay = 30000)
+    public void sendDailyMessege(){
+        simpleEmailService.send(simpleEmailService.createDailyMessage(new Mail(adminConfig.getAdminMail(),"task report","you got " + taskRepository.count() + " tasks to do","michaljanas1991@gmail.com")));
     }
 }
